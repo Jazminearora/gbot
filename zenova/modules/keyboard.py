f182294e7e44f4f57f59ecec3fcbd3d85dd74c7b
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2024-04-20 21:26:20 +0530

    Change is\_served\_user function to be asynchronous in referdb.py Use the updated is\_served\_user function in keyboard.py to check if the user has already been referred. Update the referral\_count

--- zenova/modules/keyboard.py
+++ zenova/modules/keyboard.py
@@ -112,7 +112,7 @@ async def start_command(client, message):
                 if command_parts:
                     referer_user_id = int(command_parts[1].replace("r", ""))
                     # Check if the sender user ID has already been referred
-                    is_referred = is_served_user(user_id)
+                    is_referred = await is_served_user(user_id)
                     if not is_referred:
                         # Check if the user is already registered
                         is_registered = await is_user_registered(user_id)
@@ -122,7 +122,7 @@ async def start_command(client, message):
                             await message.reply_text(f"You are successfully refered by {name}. \n\nPlease register now for using bot by command: /register")
                             referer_lang = find_language(referer_user_id)
                             referred_name = await get_user_name(user_id)
-                            total_refer = referral_count(referer_user_id)
+                            total_refer = await referral_count(referer_user_id)
                             caption = f"You have successfully referred to {referred_name}.\n\n Total refers: {total_refer}"
                             if referer_lang == "English":
                                 await zenova.send_message(referer_user_id, caption)
