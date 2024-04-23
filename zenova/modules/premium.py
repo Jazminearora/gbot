89b88800581400f48562e68a9d794032b14543c9
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2024-04-23 14:39:09 +0530

    "Add premium features and related functionality for users; update premium option button"
    This commit adds premium features and related functionality for users, including different durations of premium access with corresponding prices. It also updates the premium option button to provide users with

--- zenova/modules/premium.py
+++ zenova/modules/premium.py
@@ -1,9 +1,19 @@
 from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup
 import re
+
+
 from zenova import zenova 
+from helpers.helper import find_language
+from helpers.get_msg import get_premium_msg
+
 
 
 button_pattern = re.compile(r"^💎 (Premium|Премиум|Premium) 💎$")
 @zenova.on_message(filters.private & filters.regex(button_pattern))
 async def premium_option(client, message):
-    await message.reply_text("You selected Premium option.")
+    user_id = message.from_user.id
+    language = find_language(user_id)
+    message_text, inline_buttons = await get_premium_msg(language)
+    inline_keyboard = [buttons for buttons in inline_buttons]
+    await message.reply_text(message_text, reply_markup=InlineKeyboardMarkup(inline_keyboard))
