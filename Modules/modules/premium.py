95aa1365e1ab336f268490390ff367259bdc500b
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2024-04-25 10:34:51 +0530

    "Add referral link sharing feature for premium free users and update premium free command handler"
    This commit adds a new feature for premium free users where they can share their referral link and receive VIP status for each invited user. The premium

--- Modules/modules/premium.py
+++ Modules/modules/premium.py
@@ -6,10 +6,18 @@ import urllib.parse
 
 from Modules import cbot , BOT_USERNAME
 from helpers.helper import find_language
+from helpers.translator import translate_text
 from langdb.get_msg import get_premium_msg
 from database.referdb import get_point
 from helpers.forcesub import subscribed, user_registered
 
+async def get_text(total_points, referral_link, language)
+    msg=f"Invite users using your link and receive 👑VIP status for 1 hour for each!\n\nInvited:"
+    msg2 =f"Your personal link:\n👉"
+    translations = {"English": msg, "Russian": translate_text(msg, target_language="ru"), "Azerbejani": translate_text(msg, target_language="az")}
+    translations = {"English": msg2, "Russian": translate_text(msg2, target_language="ru"), "Azerbejani": translate_text(msg2, target_language="az")}
+    message =  translations.get(language, msg) + total_points + f"\n\n" + translations.get(language, msg2) + referral_link
+    return message
 
 button_pattern = re.compile(r"^💎 (Premium|Премиум|Premium) 💎$")
 @cbot.on_message(filters.private & filters.regex(button_pattern))
@@ -37,7 +45,21 @@ async def handle_premium_free_request(update):
 
 @cbot.on_message(filters.command("referals") & subscribed & user_registered)
 async def premium_free_command_handler(bot, update):
-    await handle_premium_free_request(update)
+    user_id = update.from_user.id
+    user_lang = find_language(user_id)
+    total_points = await get_point(user_id)
+    referral_link = f"https://t.me/{BOT_USERNAME}?start=r{user_id}"
+    share_txt = "Hey buddy!!\n\n Try this amazing bot for getting connected with strangers from the world!"
+    encoded_share_txt = urllib.parse.quote(share_txt)
+    share_link = f"https://t.me/share/url?url={referral_link}&text={encoded_share_txt}"
+    print("share link:", share_link)
+    message = await get_text(total_points, referral_link, user_lang)
+    await update.reply_text(
+        text=message,
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("Refer your Friend", url=share_link)]]
+        )
+    )
 
 @cbot.on_callback_query(filters.regex("^premium_free$") & subscribed & user_registered)
 async def premium_free_callback(bot, update):
