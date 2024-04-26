3b587ae399366add2c583072f49cdea52931f49a
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2024-04-26 20:50:58 +0530

    Update search.py: change regex pattern for search command
    The regex pattern for the search command has been updated to include a leading and trailing emoji. This change was made to improve the readability and consistency of the command. The rest of

--- Modules/modules/search.py
+++ Modules/modules/search.py
@@ -34,7 +34,7 @@ async def hlo(client, message):
     text = "Searching users:\n" + str(searching_users) + "\n\nChat pairs:\n" + str(chat_pairs)
     await message.reply(text)
 
-button_pattern = re.compile(r"^(Search for an interlocutor|Найти собеседника|Məqalə axtar) 🔎)$")
+button_pattern = re.compile(r"^(🔍 (Search for an interlocutor|Найти собеседника|Məqalə axtar) 🔎)$")
 
 @cbot.on_message(filters.private & filters.regex(button_pattern))
 async def search_interlocutor(client, message):
