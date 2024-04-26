cbc46e6c5939ec7a1c844843ec33f085a0feb058
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2024-04-26 08:44:27 +0530

    "Add get\_age\_groups\_text function and update gender and age group selection to use edit\_caption instead of reply"
    This commit adds a new function, get\_age\_groups\_text, which retrieves the age groups selected

--- Modules/modules/configure.py
+++ Modules/modules/configure.py
@@ -9,6 +9,13 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 button_pattern = re.compile(r"^(🔧 (Configure search|Customize search|Modify search|Setup search) 🔧)$")
 
+async def get_age_groups_text(user_id):
+    age_groups_list = await vip_users_details(user_id, "age_groups")
+    if age_groups_list:
+        return "\n".join([f"• {age_group}" for age_group in age_groups_list])
+    else:
+        return "No age groups selected yet."
+
 @cbot.on_message(((filters.private & filters.regex(button_pattern)) | (filters.command("configure"))) & subscribed & user_registered)
 async def configure_search(client, message):
     markup = InlineKeyboardMarkup([
@@ -22,12 +29,11 @@ async def configure_search(client, message):
 async def gender_callback(client, callback_query):
     markup = InlineKeyboardMarkup([
         [InlineKeyboardButton("Male", callback_data="cmle"),
-         InlineKeyboardButton("Female", callback_data="cfem"),
-         InlineKeyboardButton("Other", callback_data="cother")],
+         InlineKeyboardButton("Female", callback_data="cfem")],
         [InlineKeyboardButton("Any gender", callback_data="cany_gndr"),
          InlineKeyboardButton("Go back", callback_data="cgoback")]
     ])
-    await callback_query.message.reply("Please select your gender:", reply_markup=markup)
+    await callback_query.message.edit_caption("Please select your gender:", reply_markup=markup)
 
 @cbot.on_callback_query(filters.regex("cmle"))
 async def male_callback(client, callback_query):
@@ -35,9 +41,9 @@ async def male_callback(client, callback_query):
     is_premium, _ = await is_user_premium(user_id)
     if is_premium:
         await save_premium_user(user_id, gender="male")
-        await callback_query.message.reply("Your gender has been updated to male.")
+        await callback_query.answer("Your gender has been updated to male.", show_alert=True)
     else:
-        await callback_query.message.reply("You need to be a premium user to update your gender.")
+        await callback_query.answer("You need to be a premium user to update your gender.", show_alert=True)
 
 @cbot.on_callback_query(filters.regex("cfem"))
 async def female_callback(client, callback_query):
@@ -45,19 +51,9 @@ async def female_callback(client, callback_query):
     is_premium, _ = await is_user_premium(user_id)
     if is_premium:
         await save_premium_user(user_id, gender="female")
-        await callback_query.message.reply("Your gender has been updated to female.")
+        await callback_query.answer("Your gender has been updated to female.", show_alert=True)
     else:
-        await callback_query.message.reply("You need to be a premium user to update your gender.")
-
-@cbot.on_callback_query(filters.regex("cother"))
-async def other_gender_callback(client, callback_query):
-    user_id = callback_query.from_user.id
-    is_premium, _ = await is_user_premium(user_id)
-    if is_premium:
-        await save_premium_user(user_id, gender="other")
-        await callback_query.message.reply("Your gender has been updated to other.")
-    else:
-        await callback_query.message.reply("You need to be a premium user to update your gender.")
+        await callback_query.answer("You need to be a premium user to update your gender.", show_alert=True)
 
 @cbot.on_callback_query(filters.regex("cany_gndr"))
 async def any_gender_callback(client, callback_query):
@@ -65,9 +61,9 @@ async def any_gender_callback(client, callback_query):
     is_premium, _ = await is_user_premium(user_id)
     if is_premium:
         await save_premium_user(user_id, gender="any gender")
-        await callback_query.message.reply("Your gender has been updated to any gender.")
+        await callback_query.answer("Your gender has been updated to any gender.", show_alert=True)
     else:
-        await callback_query.message.reply("You need to be a premium user to update your gender.")
+        await callback_query.answer("You need to be a premium user to update your gender.", show_alert=True)
 
 @cbot.on_callback_query(filters.regex("cgoback"))
 async def cback_callback(client, callback_query):
@@ -76,18 +72,17 @@ async def cback_callback(client, callback_query):
          InlineKeyboardButton("Age", callback_data="cage"),
          InlineKeyboardButton("Room", callback_data="crm")]
     ])
-    await callback_query.message.reply("Please select an option:", reply_markup=markup)
+    await callback_query.message.edit_caption("Please select your gender:", reply_markup=markup)
 
 @cbot.on_callback_query(filters.regex("cage"))
 async def age_callback(client, callback_query):
     markup = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Below 18", callback_data="cblw_18"),
-         InlineKeyboardButton("18-24", callback_data="c18_24"),
-         InlineKeyboardButton("25-34", callback_data="c25_34"),
-         InlineKeyboardButton("Above 35", callback_data="cabv_35"),
-         InlineKeyboardButton("Go back", callback_data="agoback")]
-    ])
-    await callback_query.message.reply("Please select your age group(s):", reply_markup=markup)
+            [InlineKeyboardButton("Gender", callback_data="cgndr"),
+            InlineKeyboardButton("Age", callback_data="cage"),
+            InlineKeyboardButton("Room", callback_data="crm")]
+            ])
+    await callback_query.message.edit_caption("Please select an option:", reply_markup=markup)
+
 
 age_groups = {}
 
@@ -98,7 +93,7 @@ async def age_group_callback(client, callback_query):
     if user_id not in age_groups:
         age_groups[user_id] = []
     age_groups[user_id].append(age_group)
-    await callback_query.answer("Age group added!")
+    await callback_query.answer("Age group added!", show_alert=True)
 
 @cbot.on_callback_query(filters.regex("agoback"))
 async def back_callback(client, callback_query):
@@ -108,6 +103,14 @@ async def back_callback(client, callback_query):
         is_premium, _ = await is_user_premium(user_id)
         if is_premium:
             await save_premium_user(user_id, age_groups=age_groups_list)
-            await callback_query.message.reply("Your age groups have been updated.")
+            await callback_query.answer("Your age groups have been updated.", show_alert=True)
+            markup = InlineKeyboardMarkup([
+            [InlineKeyboardButton("Gender", callback_data="cgndr"),
+            InlineKeyboardButton("Age", callback_data="cage"),
+            InlineKeyboardButton("Room", callback_data="crm")]
+            ])
+            await callback_query.message.edit_caption("Please select an option:", reply_markup=markup)
+
         else:
-            await callback_query.message.reply("You need to be a premium user to update your age groups.")
+            await callback_query.answer("You need to be a premium user to update your age groups.", show_alert=True)
+
