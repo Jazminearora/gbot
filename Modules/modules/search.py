2e7295c9f14c7f5201d9412ce25c8bd35a70caea
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2024-04-30 23:28:39 +0530

    "Added checks for None in check\_inactive\_chats function and corrected print statement"
    This commit includes changes to the `check_inactive_chats` function in the `search.py` module. Specifically, it

--- Modules/modules/search.py
+++ Modules/modules/search.py
@@ -272,6 +272,8 @@ async def forward_message(client, message):
 # function to check for inactive chats
 async def check_inactive_chats():
     for pair in chat_pairs:
+        if not pair:
+            return
         user1, user2 = pair
         msg_time1 = message_timestamps.get(f"user_{user1}")
         msg_time2 = message_timestamps.get(f"user_{user2}")
@@ -280,7 +282,7 @@ async def check_inactive_chats():
         cr_time = datetime.strptime(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()), "%Y-%m-%d %H:%M:%S")
         print("ww", last_message_time1, last_message_time2)
         if last_message_time1 and last_message_time2:
-            print(cr_time - last_message_time1).seconds 
+            print((cr_time - last_message_time1).seconds)
             if (cr_time - last_message_time1).seconds > 60 and (cr_time - last_message_time2).seconds() > 60:
                 # Chat has been inactive for more than 10 minutes, end the chat
                 delete_pair(user1)
