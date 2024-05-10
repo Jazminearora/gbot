210bdf41c995202e23541ba93638086f7cf3c02e
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2024-05-10 15:50:26 +0530

    "Update weekly\_gw function to send a message about weekly winners to log group; add comments and variables in test.py"
    This commit updates the weekly\_gw function to send a message about the weekly winners to the log group

--- Modules/modules/weeklyGW.py
+++ Modules/modules/weeklyGW.py
@@ -1,5 +1,6 @@
 from pytz import timezone
 from pyrogram.errors import PeerIdInvalid
+from datetime import timedelta
 from apscheduler.triggers.cron import CronTrigger
 
 from database.premiumdb import get_top_chat_users, extend_premium_user_hrs
@@ -13,7 +14,10 @@ from config import LOG_GROUP
 async def weekly_gw():
     try:
         topper = get_top_chat_users()
-
+        st_win1 = {}
+        st_win2 = {}
+        st_win3 = {}
+        formatted_chat_time = str(timedelta(seconds=user["chat_time"]))
         # Distribute premium to top 3 users
         for i, user in enumerate(topper):
             user_id = user['user_id']
@@ -23,10 +27,13 @@ async def weekly_gw():
             # Distribute premium based on the rank
             if i == 0:
                 days = 3
+                st_win1.update({"user_id": user_id, "chat_time":chat_time})
             elif i == 1:
                 days = 2
+                st_win2.update({"user_id": user_id, "chat_time":chat_time})
             elif i == 2:
                 days = 1
+                st_win3.update({"user_id": user_id, "chat_time":chat_time})
 
             # Extend the user's premium subscription
             extend_premium_user_hrs(int(user_id), days * 24)
@@ -60,7 +67,8 @@ async def weekly_gw():
             except Exception as e:
                 await cbot.send_message(LOG_GROUP, text= f"{e}\n\nError occured in sending winning message to winner! Premium membership activated for the user! User details-\n\nUser id = {user_id}\nPosition = {place}\nChat time = {chat_time}")
                 pass
-
+        # send a message to log group about weekly winners  
+        await cbot.send_message(LOG_GROUP, f"**WEEKLY WINNER ANNOUNCEMENT**\n\n--1st Position--\nUser_id: {st_win1.get("user_id")}\nWeek chat time:{st_win1.get("chat_time")}\n\n--2nd Position--\nUser_id: {st_win2.get("user_id")}\nWeek chat time:{st_win2.get("chat_time")}\n\n--3rd Position--\nUser_id: {st_win3.get("user_id")}\nWeek chat time:{st_win3.get("chat_time")}")
         # Reset the user's chat time
         reset_chatime()
 
