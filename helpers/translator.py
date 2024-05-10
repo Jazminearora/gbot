acdde796bcb359073dca417039313298a35780be
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2024-05-10 21:35:50 +0530

    "Update translate\_async function to handle exceptions and add print statement; no changes to translate\_text function"
    The diff includes updates to the translate\_async function in the translator.py file. The function now includes a try-except

--- helpers/translator.py
+++ helpers/translator.py
@@ -18,18 +18,18 @@ def translate_text(text, target_language):
         return None
     
 async def translate_async(text, target_language):
-    # try:
-    if target_language == 'English':
-        tr_lang = "en"
-    elif target_language == "Russian":
-        tr_lang = "ru"
-    elif target_language == "Azerbejani":
-        tr_lang = "az"
-    else:
-        tr_lang = target_language
-    t = Translator()
-    translation = await t.translate(text, targetlang=tr_lang)
-    return translation.text
-    # except Exception as e:
-    #     print(target_language)
-    #     return f'Error occurred during translation: {e}'
+    try:
+        if target_language == 'English':
+            tr_lang = "en"
+        elif target_language == "Russian":
+            tr_lang = "ru"
+        elif target_language == "Azerbejani":
+            tr_lang = "az"
+        else:
+            tr_lang = target_language
+        t = Translator()
+        translation = await t.translate(text, targetlang=tr_lang)
+        return translation.text
+    except Exception as e:
+        print(target_language, text)
+        return f'Error occurred during translation: {e}'
