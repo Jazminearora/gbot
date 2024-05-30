1193463333eefccec9029af9d3bade6d628b87e9
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2024-05-30 23:11:10 +0530

    Remove `user_registered` filter from `/rules` and `/help` command handlers.

--- Modules/modules/user_cmd.py
+++ Modules/modules/user_cmd.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
 from helpers.helper import find_language
-from helpers.filters import subscribed, user_registered
+from helpers.filters import subscribed
 from langdb.rules import get_rules
 from langdb.help_msg import get_help_msg
 
@@ -8,7 +8,7 @@ from Modules import cbot
 
 
 # handle command /rules
-@cbot.on_message(filters.command(["rules"]) & filters.private & subscribed & ~user_registered)
+@cbot.on_message(filters.command(["rules"]) & filters.private & subscribed)
 async def rules(client, message):
     user_id = message.from_user.id
     language = find_language(user_id)
@@ -17,7 +17,7 @@ async def rules(client, message):
 
 
 # handle command /help
-@cbot.on_message(filters.command(["help"]) & filters.private & subscribed & ~user_registered)
+@cbot.on_message(filters.command(["help"]) & filters.private & subscribed)
 async def help(client, message):
     user_id = message.from_user.id
     language = find_language(user_id)
