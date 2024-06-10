4d7261315e98688bc579d11de8429b6b321aa4ec
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2024-06-10 18:13:29 +0530

    "Updated unfriend_callback and unfriend_confirm functions in freinds.py module"

--- Modules/modules/freinds.py
+++ Modules/modules/freinds.py
@@ -126,31 +126,39 @@ async def unfriend_callback(_, callback_query):
     language = find_language(user_id)
     friend_id = int(callback_query.data.split("_")[1])
     frens_list = vip_users_details(user_id, "frens")
-    if frens_list is None or friend_id not in frens_list:
+    if frens_list is None or all(friend["friend_id"]!= user_id for friend in frens_list):
         await callback_query.answer(await translate_async("This user is not your friend.", language), show_alert=True)
         return
-    confirm_text = await translate_async("Are you sure, you want to unfriend {}?", language)
+    confirm_text = await translate_async("Are you sure, you want to unfriend him?", language)
     
     reply_markup= InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("Yes", callback_data=f"unfriend_confirm_{friend_id}"),
-                    InlineKeyboardButton("No", callback_data="unfriend_cancel"),
+                    InlineKeyboardButton(await translate_async("Yes 💔", language), callback_data=f"unfriend_confirm_{friend_id}"),
+                    InlineKeyboardButton(await translate_async("No ❤️", language), callback_data="unfriend_cancel"),
                 ]
             ]
         )
 
-    await callback_query.message.edit_text(confirm_text.format(friend_id), reply_markup= reply_markup)
+    await callback_query.message.edit_text(confirm_text, reply_markup= reply_markup)
 
 @cbot.on_callback_query(filters.regex("unfriend_confirm_"))
 async def unfriend_confirm(client, query):
     friend_id = int(query.data.split("_")[2])
     user_id = query.from_user.id
     language = find_language(user_id)
-    await query.message.edit_text(await translate_async("You have unfriended the user!", language))
-    remove_item_from_field(user_id, "frens", friend_id)
-    remove_item_from_field(friend_id, "frens", user_id)
-    await cbot.send_message(friend_id, await translate_async("You have been unfriended by your one of friend!", language))
+    frens_list1 = vip_users_details(user_id, "frens")
+    for friend in frens_list1:
+        if friend["friend_id"] == friend_id:
+            name1 = friend["name"]
+    frens_list2 = vip_users_details(friend_id, "frens")
+    for friend in frens_list2:
+        if friend["friend_id"] == user_id:
+            name2 = friend["name"]
+    await query.message.edit_text(f"{await translate_async(f"You have unfriended", language)} {name1}!")
+    remove_item_from_field(user_id, "frens", {"friend_id": friend_id, "nickname": name1})
+    remove_item_from_field(friend_id, "frens", {"friend_id": user_id, "nickname": name2})
+    await cbot.send_message(friend_id, f"{await translate_async("You have been unfriended by", language)} {name2}!")
 
 
 @cbot.on_callback_query(filters.regex("unfriend_cancel"))
